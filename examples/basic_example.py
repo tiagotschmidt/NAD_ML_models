eac18e8b9e97968c1abf4a4c2e281edaccf06c6a
Tiago Schmidt <68293535+tiagotschmidt@users.noreply.github.com>
2025-06-16 18:56:37 -0300

    Adding basic example (MPL) for new EPPNAD version.

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -4,70 +4,73 @@ from keras._tf_keras.keras.models import Sequential
 from keras._tf_keras.keras.layers import Dense
 import pandas as pd
 
+from eppnad.core.manager import profile
+from eppnad.utils.framework_parameters import (
+    Lifecycle,
+    PercentageRangeParameter,
+    Platform,
+    ProfileMode,
+    RangeParameter,
+)
+
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
 os.environ["GRPC_VERBOSITY"] = "ERROR"
 os.environ["GLOG_minloglevel"] = "2"
 
 
-# def first_layer(model: keras.models.Model, number_of_units, input_shape):
-#     model.add(Dense(units=number_of_units, input_dim=input_shape, activation="relu"))
+def first_layer(model: keras.models.Model, number_of_units, input_shape):
+    model.add(Dense(units=number_of_units, input_dim=input_shape, activation="relu"))
 
 
-# def repeated_layer(model: keras.models.Model, number_of_units, input_shape):
-#     model.add(Dense(units=number_of_units, input_dim=input_shape, activation="relu"))
+def repeated_layer(model: keras.models.Model, number_of_units, input_shape):
+    model.add(Dense(units=number_of_units, input_dim=input_shape, activation="relu"))
 
 
-# def final_layer(model: keras.models.Model):
-#     model.add(Dense(units=1, activation="sigmoid"))
+def final_layer(model: keras.models.Model):
+    model.add(Dense(units=1, activation="sigmoid"))
 
 
-numbers_of_layers = RangeParameter(
-    1, 3, 1, FrameworkParameterType.NumberOfLayers, RangeMode.Additive
+numbers_of_layers = RangeParameter([1])
+numbers_of_units = RangeParameter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100])
+numbers_of_epochs = RangeParameter([100])
+numbers_of_features = RangeParameter([93])
+sampling_rates = PercentageRangeParameter([1])
+
+# Define profile mode
+profile_mode = ProfileMode(
+    Lifecycle.TRAIN_AND_TEST,
+    train_platform=Platform.GPU,
+    test_platform=Platform.CPU,
 )
-# numbers_of_units = RangeParameter(
-#     10, 210, 50, FrameworkParameterType.NumberOfNeurons, RangeMode.Additive
-# )
-# numbers_of_epochs = RangeParameter(
-#     90, 90, 20, FrameworkParameterType.NumberOfEpochs, RangeMode.Additive
-# )
-# numbers_of_features = RangeParameter(
-#     13, 93, 20, FrameworkParameterType.NumberOfFeatures, RangeMode.Additive
-# )
 
-# # Define profile mode
-# profile_mode = MLMode(
-#     LifecycleSelected.TrainAndTest,
-#     train_platform=Platform.GPU,
-#     test_platform=Platform.CPU,
-# )
+# Define other parameters
+number_of_samples = 10
+batch_size = 8192 * 2
+performance_metrics_list = ["precision", "f1_score", "recall"]
+preprocessed_dataset = pd.read_csv("data/NSL-KDD/preprocessed_binary_dataset.csv")
+dataset_target_label = "intrusion"
+loss_metric_str = "binary_crossentropy"
+optimizer = "adam"
 
-# # Define other parameters
-# number_of_samples = 10
-# batch_size = 8192
-# performance_metrics_list = ["precision", "f1_score", "recall"]
-# preprocessed_dataset = pd.read_csv("dataset/preprocessed_binary_dataset.csv")
-# dataset_target_label = "intrusion"
-# loss_metric_str = "binary_crossentropy"
-# optimizer = "adam"
 
-# profile(
-#     Sequential,
-#     "MLP_geral",
-#     first_custom_layer_code=first_layer,
-#     repeated_custom_layer_code=repeated_layer,
-#     final_custom_layer_code=final_layer,
-#     numbers_of_layers=numbers_of_layers,
-#     numbers_of_units=numbers_of_units,
-#     numbers_of_epochs=numbers_of_epochs,
-#     numbers_of_features=numbers_of_features,
-#     profile_mode=profile_mode,
-#     number_of_samples=number_of_samples,
-#     batch_size=batch_size,
-#     sampling_rate=1,
-#     performance_metrics_list=performance_metrics_list,
-#     preprocessed_dataset=preprocessed_dataset,
-#     dataset_target_label=dataset_target_label,
-#     loss_metric_str=loss_metric_str,
-#     optimizer=optimizer,
-# )
+profile(
+    Sequential,
+    "MLP_neurons",
+    first_custom_layer_code=first_layer,
+    repeated_custom_layer_code=repeated_layer,
+    final_custom_layer_code=final_layer,
+    layers=numbers_of_layers,
+    units=numbers_of_units,
+    epochs=numbers_of_epochs,
+    features=numbers_of_features,
+    profile_mode=profile_mode,
+    statistical_samples=number_of_samples,
+    batch_size=batch_size,
+    sampling_rates=sampling_rates,
+    performance_metrics=performance_metrics_list,
+    dataset=preprocessed_dataset,
+    target_label=dataset_target_label,
+    loss_function=loss_metric_str,
+    optimizer=optimizer,
+)
